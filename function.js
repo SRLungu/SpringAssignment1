c7c46975c79123bb8f9ac40624ed821b1ad631ed
Lungu SR <202250143@ufh.ac.za>
2024-04-12 12:54:01 +0200

    implementing first spring boot application as an assignment. This application has a method that return 2 foundation courses, 3 undergraduate courses and 4 honours courses in the computer science department.

---
+++ function.js
@@ -0,0 +1,12 @@
+function goTopage3(){
+    window.location.href = "foundation.html"
+}
+function goTopage2(){
+    window.location.href = "undergraduate.html"
+}
+function goTopage4(){
+    window.location.href = "honours.html"
+}
+function goTopage1(){
+    window.location.href = "allcourses.html"
+}
